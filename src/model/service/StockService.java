883fd90b005f6033a2d909ea5bb0b0f89ad51c82
KangChangSeon <rlckdtjs@naver.com>
2025-03-18 09:26:09 +0900

    [refactor] : boolean 메서드 이름 수정
    컨벤션 규칙에 따라 check~ 가 아닌 is 로 변경

--- src/model/service/StockService.java
+++ src/model/service/StockService.java
@@ -5,7 +5,9 @@ import model.dto.StockDTO;
 import java.util.List;
 
 public interface StockService {
-    boolean checkIncomingStock(int incoming_num);
-    boolean checkIncomingStockHistory(int incoming_num);
+    boolean isIncomingStockUpdated(int incoming_num);
+    boolean isIncomingStockHistoryUpdated(int incoming_num);
+    boolean isOutgoingStockUpdated(int outgoing_num);
+    boolean isOutgoingStockHistoryUpdated(int outgoing_num);
     List<StockDTO> getUserStock(int user_id);
 }
--- src/model/service/StockServiceImpl.java
+++ src/model/service/StockServiceImpl.java
@@ -12,7 +12,7 @@ public class StockServiceImpl implements StockService {
     private StockDAO stockDAO = new StockDAOImpl();
 
     // 입고 승인(완료) 시점에 프로시저를 호출하여 재고와 이력 업데이트
-    public boolean checkIncomingStock(int incoming_num) {
+    public boolean isIncomingStockUpdated(int incoming_num) {
         boolean result = stockDAO.incomingUpdateStock(incoming_num);
         if (result) {
             System.out.println("재고 업데이트 완료 : 재고 업데이트 완료.");
@@ -23,7 +23,7 @@ public class StockServiceImpl implements StockService {
     }
 
     @Override
-    public boolean checkIncomingStockHistory(int incoming_num) {
+    public boolean isIncomingStockHistoryUpdated(int incoming_num) {
         boolean result = stockDAO.incomingUpdateStockHistory(incoming_num);
         if (result) {
             System.out.println("재고 이력 업데이트 완료 : 재고 이력 업데이트 완료.");
@@ -33,7 +33,29 @@ public class StockServiceImpl implements StockService {
         return result;
     }
 
+    @Override
+    public boolean isOutgoingStockUpdated(int outgoing_num) {
+        boolean result = stockDAO.outgoingUpdateStock(outgoing_num);
+        if (result) {
+            System.out.println("재고 업데이트 완료 : 재고 업데이트 완료.");
+        } else {
+            System.err.println("재고 업데이트 실패 : 재고 업데이트 에러 발생.");
+        }
+        return result;
+    }
+
+    @Override
+    public boolean isOutgoingStockHistoryUpdated(int outgoing_num) {
+        boolean result = stockDAO.incomingUpdateStockHistory(outgoing_num);
+        if (result) {
+            System.out.println("재고 이력 업데이트 완료 : 재고 이력 업데이트 완료.");
+        } else {
+            System.err.println("재고 이력 업데이트 실패 : 재고 이력 업데이트 에러 발생.");
+        }
+        return result;
+    }
+
     public List<StockDTO> getUserStock(int user_id) {
-        return stockDAO.checkStock(user_id).orElseGet(ArrayList::new);
+        return stockDAO.checkUserStock(user_id).orElseGet(ArrayList::new);
     }
 }
