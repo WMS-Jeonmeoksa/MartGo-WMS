450e1441f7cd26ae9bfa78d484024340f8551d05
KangChangSeon <rlckdtjs@naver.com>
2025-03-19 11:51:20 +0900

    feat(Method) : 추가 기능 필요로 인한 메서드 구현
    총 관리자와 창고 관리자에 따라 출력 할 수 있는 목록을 차별화 하기 위해 메서드를 추가로 구현하였음

--- src/model/dao/StockDAOImpl.java
+++ src/model/dao/StockDAOImpl.java
@@ -55,13 +55,18 @@ public class StockDAOImpl implements StockDAO {
         return checkStockList;
     }
 
+
     @Override
-    public List<StockDTO> checkAllStock(String admin_id) {
+    public List<StockDTO> checkAdminUserStock(String admin_id) {
         List<StockDTO> checkStockList = new ArrayList<>();
         ResultSet rs = null;
         try {
             conn = DbUtil.getConnection();
-            pstmt = conn.prepareStatement("SELECT * FROM stock");
+            pstmt = conn.prepareStatement("SELECT stock_num,count, total_price, stock.user_id, product_id, sector_id, stock.warehouse_id FROM stock\n" +
+                    "    JOIN user ON user.user_id = stock.user_id\n" +
+                    "    JOIN admin ON user.admin_id = admin.admin_id\n" +
+                    "WHERE stock.user_id = user.user_id AND user.admin_id = ?");
+            pstmt.setString(1, admin_id);
             rs = pstmt.executeQuery();
 
             while (rs.next()) {
@@ -93,16 +98,64 @@ public class StockDAOImpl implements StockDAO {
     }
 
     @Override
-    public List<StockHistoryDTO> checkStockHistoryList() {
+    public List<StockDTO> checkGeneralStock(String admin_id) {
+        List<StockDTO> checkStockList = new ArrayList<>();
+        ResultSet rs = null;
+        try {
+            conn = DbUtil.getConnection();
+            pstmt = conn.prepareStatement("SELECT stock_num,count, total_price, stock.user_id, product_id, sector_id, stock.warehouse_id\n" +
+                    "FROM stock\n" +
+                    "JOIN warehouse ON stock.warehouse_id = warehouse.warehouse_id\n" +
+                    "JOIN admin ON admin.admin_id = ?\n" +
+                    "WHERE warehouse.warehouse_id = admin.warehouse_id");
+            pstmt.setString(1, admin_id);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                StockDTO stock = StockDTO.builder()
+                        .stock_num(rs.getInt("stock_num"))
+                        .count(rs.getInt("count"))
+                        .total_price(rs.getInt("total_price"))
+                        .user_id(rs.getString("user_id"))
+                        .product_id(rs.getString("product_id"))
+                        .sector_id(rs.getString("sector_id"))
+                        .warehouse_id(rs.getInt("warehouse_id"))
+                        .build();
+                checkStockList.add(stock);
+            }
+            return checkStockList;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (pstmt != null) pstmt.close();
+                if (conn != null) conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return checkStockList;
+    }
+
+    @Override
+    public List<StockHistoryDTO> checkGeneralStockHistory(String admin_id) {
         List<StockHistoryDTO> checkStockList = new ArrayList<>();
         ResultSet rs = null;
         try {
             conn = DbUtil.getConnection();
-            pstmt = conn.prepareStatement("SELECT * FROM stock_history");
+            pstmt = conn.prepareStatement("SELECT history_num, product_id, stock_history.sector_id, count, change_date, change_type, stock_history.admin_id, incoming_num, outgoing_num, stock_num\n" +
+                    "FROM stock_history\n" +
+                    "         JOIN sector ON stock_history.sector_id = sector.sector_id\n" +
+                    "         JOIN warehouse ON warehouse.warehouse_id = sector.warehouse_id\n" +
+                    "         JOIN admin ON admin.admin_id = ?\n" +
+                    "WHERE warehouse.warehouse_id = admin.warehouse_id");
+            pstmt.setString(1, admin_id);
             rs = pstmt.executeQuery();
 
             while (rs.next()) {
-                StockHistoryDTO stockHistory = new StockHistoryDTO.StockHistoryDTOBuilder()
+                StockHistoryDTO stockHistory = StockHistoryDTO.builder()
                         .history_num(rs.getInt("history_num"))
                         .product_id(rs.getString("product_id"))
                         .sector_id(rs.getString("sector_id"))
@@ -131,4 +184,49 @@ public class StockDAOImpl implements StockDAO {
         }
         return checkStockList;
     }
+
+    @Override
+    public List<StockHistoryDTO> checkAdminStockHistory(String admin_id) {
+        List<StockHistoryDTO> checkStockList = new ArrayList<>();
+        ResultSet rs = null;
+        try {
+            conn = DbUtil.getConnection();
+            pstmt = conn.prepareStatement("SELECT history_num, product_id, stock_history.sector_id, count, change_date, change_type, stock_history.admin_id, incoming_num, outgoing_num, stock_num\n" +
+                    "FROM stock_history\n" +
+                    "JOIN admin ON admin.admin_id = ?");
+            pstmt.setString(1, admin_id);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                StockHistoryDTO stockHistory = StockHistoryDTO.builder()
+                        .history_num(rs.getInt("history_num"))
+                        .product_id(rs.getString("product_id"))
+                        .sector_id(rs.getString("sector_id"))
+                        .count(rs.getInt("count"))
+                        .change_date(rs.getDate("change_date"))
+                        .change_type(rs.getString("change_type"))
+                        .admin_id(rs.getString("admin_id"))
+                        .incoming_num(rs.getInt("incoming_num"))
+                        .outgoing_num(rs.getInt("outgoing_num"))
+                        .stock_num(rs.getInt("stock_num"))
+                        .build();
+                checkStockList.add(stockHistory);
+            }
+            return checkStockList;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (pstmt != null) pstmt.close();
+                if (conn != null) conn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return checkStockList;
+    }
+
+
 }
--- src/model/service/StockService.java
+++ src/model/service/StockService.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public interface StockService {
     List<StockDTO> getUserStock(String user_id);
-    List<StockDTO> getAllStock(String admin_id);
-    List<StockHistoryDTO> getStockHistoryList();
+    List<StockDTO> getAdminUserStock(String admin_id);
+    List<StockDTO> getGeneralStock(String admin_id);
+    List<StockHistoryDTO> getAdminStockHistory(String admin_id);
+    List<StockHistoryDTO> getGeneralStockHistory(String admin_id);
 }
