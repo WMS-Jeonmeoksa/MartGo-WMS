41b49d34f176bccee54b39d708c5ecfd7d5f9b43
KangChangSeon <rlckdtjs@naver.com>
2025-03-19 09:31:12 +0900

    refactor(Method) : 컨트롤러에서 사용할 메서드 구현
    DAO, Service 에서 불러와야한 메서드를 컨트롤러 main에서 바로 불렀는데 메서드로 구현함

--- src/controller/StockController.java
+++ src/controller/StockController.java
@@ -9,37 +9,38 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.List;
 
 public class StockController {
+    StockService stockService = new StockServiceImpl();
 
-    public static void main(String[] args) {
-        StockService stockService = new StockServiceImpl();
+    public void printUserStock(String user_id) {
+        List<StockDTO> userStock = stockService.getUserStock(user_id);
+        userStock.forEach(stock -> {
+            System.out.println("Stock 번호: " + stock.getStock_num());
+            System.out.println("회원 ID " + stock.getUser_id());
+            System.out.println("제품 ID: " + stock.getProduct_id());
+            System.out.println("수량: " + stock.getCount());
+            System.out.println("총가격: " + stock.getTotal_price());
+            System.out.println("섹터 ID: " + stock.getSector_id());
+            System.out.println("창고 ID: " + stock.getWarehouse_id());
+            System.out.println("-----------------------------");
+        });
+    }
 
-        // 입고 완료 되었을 때 해당 회원 id 를 넘겨받음
-//        String user_id = "U123";
-//        List<StockDTO> userStock = stockService.getUserStock(user_id);
-//        userStock.forEach(stock -> {
-//            System.out.println("Stock 번호: " + stock.getStock_num());
-//            System.out.println("회원 ID " + stock.getUser_id());
-//            System.out.println("제품 ID: " + stock.getProduct_id());
-//            System.out.println("수량: " + stock.getCount());
-//            System.out.println("총가격: " + stock.getTotal_price());
-//            System.out.println("섹터 ID: " + stock.getSector_id());
-//            System.out.println("창고 ID: " + stock.getWarehouse_id());
-//            System.out.println("-----------------------------");
-//        });
 
-//        String admin_id = "A123";
-//        List<StockDTO> allStock = stockService.getAllStock(admin_id);
-//        allStock.forEach(stock -> {
-//            System.out.println("Stock 번호: " + stock.getStock_num());
-//            System.out.println("회원 ID " + stock.getUser_id());
-//            System.out.println("제품 ID: " + stock.getProduct_id());
-//            System.out.println("수량: " + stock.getCount());
-//            System.out.println("총가격: " + stock.getTotal_price());
-//            System.out.println("섹터 ID: " + stock.getSector_id());
-//            System.out.println("창고 ID: " + stock.getWarehouse_id());
-//            System.out.println("-----------------------------");
-//        });
+    public void printAllStock(String admin_id) {
+        List<StockDTO> allStock = stockService.getAllStock(admin_id);
+        allStock.forEach(stock -> {
+            System.out.println("Stock 번호: " + stock.getStock_num());
+            System.out.println("회원 ID " + stock.getUser_id());
+            System.out.println("제품 ID: " + stock.getProduct_id());
+            System.out.println("수량: " + stock.getCount());
+            System.out.println("총가격: " + stock.getTotal_price());
+            System.out.println("섹터 ID: " + stock.getSector_id());
+            System.out.println("창고 ID: " + stock.getWarehouse_id());
+            System.out.println("-----------------------------");
+        });
+    }
 
+    public void printAllStockHistory() {
         List<StockHistoryDTO> allStockHistory = stockService.getStockHistoryList();
         allStockHistory.forEach(stock -> {
             System.out.println("-----------------------------");
